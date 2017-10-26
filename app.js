82d2d3355788da53a79fe0bd97063dd2fcf1928b
Yinghao Wang <yinghao.a.wang@gmail.com>
2017-10-26 16:52:09 -0700

    now attempts to scrape amazon (captcha stops me)

--- app.js
+++ app.js
@@ -16,11 +16,14 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 app.use('/users', users);
--- controllers/scraperController.js
+++ controllers/scraperController.js
@@ -5,16 +5,38 @@ var async = require('async');
 
 exports.index = function(req, res, next) {
     async.parallel({
-        raw_data: function(callback) {
-            var url = 'http://imdb.com/title/tt1229340';
+        scrape_data: function(callback) {
+            var url = 'https://www.amazon.com/gp/product/B00CHSCJ7A/ref=s9u_simh_gw_i2?ie=UTF8&fpl=fresh&pd_rd_i=B00CHSCJ7A&pd_rd_r=4be7b76f-baa0-11e7-a78d-95e65c90c0fe&pd_rd_w=x8T6r&pd_rd_wg=CWGJm&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=M09M8WJGBS93ZAJC20K6&pf_rd_t=36701&pf_rd_p=1dd2ffc3-992f-4bde-81b0-de270e0ead5a&pf_rd_i=desktop';
             request(url, function(err, res, body) {
                 if (err) console.log(err);
-                callback(null, body);
+                var $ = cheerio.load(body);
+                var json = { title: "", release: "", rating: ""};
+                /*
+                $('#productTitle').filter(function() {
+                    json.title = $(this).children().text();
+                });
+                */
+                json.raw_html = body;
+                json.title = $('#productTitle').text();
+                console.log(json.title);
+                $('#wayfinding-breadcrumbs_container').filter(function() {
+                    json.release = $(this).text();
+                });
+                $('#averageCustomerReviews').filter(function() {
+                    json.rating = $(this).text();
+                });
+                console.log(json);
+                callback(null, json);
             });
         }
     }, function(err, results) {
         console.log("success");
-        res.render('index', { title: 'Index', error: err, data: results.raw_data });
+        res.render('index', {
+            title: 'Index', error: err, data: results,
+            product_title: results.scrape_data.title,
+            product_release: results.scrape_data.release,
+            product_rating: results.scrape_data.rating
+        });
     });
 };
 
