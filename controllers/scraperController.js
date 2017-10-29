70e66c17ab95c2e66f18a08d163b9f3a4376fbca
Yinghao Wang <yinghao.a.wang@gmail.com>
2017-10-29 01:05:52 -0700

    scrapes pages from the lists

--- controllers/scraperController.js
+++ controllers/scraperController.js
@@ -5,96 +5,85 @@ var async = require('async');
 var domain = "http://www.imdb.com";
 var list = "http://www.imdb.com/search/title?groups=top_250&sort=user_rating";
 
-function scrape_list(url, callback) {
-    var json = {};
+function scrape_page(json, callback) {
+    async.each(json['list'], function (item, callback) {
+        var url = item['link'];
+        request(url, function(err, res, body) {
+            if (err) console.error(err);
+            var $ = cheerio.load(body);
+            var movie = {
+                'raw_html': body,
+                'title': $('.title_wrapper').children('h1').text(),
+                'release': $('.subtext').children().last().text(),
+                'rating': $('.ratingValue').text(),
+                'rating count': $('span[itemprop="ratingCount"]').text(),
+                'genre': function() {
+                    var line = $('div[itemprop="genre"]').children().not('.inline').text();
+                    var values = line.split("| ");
+                    return values;
+                }(),
+                'content rating': function() {
+                    var line = $('.subtext').text();
+                    var value = line.substring(0, line.indexOf('|'));
+                    return value;
+                }(),
+            };
+            // Get directors, writers, and actors from /fullcredits
+            json['movies'].push(movie);
+            callback(null, json);
+        });
+        //callback(null, json);
+    }, function(err) {
+        if (err) console.error(err);
+        callback(null, json);
+    });
+}
 
+function scrape_list_page(json, callback) {
+    var url = json['next_url'];
+    const prevCount = Object.keys(json['list']).length;
+    request(url, function(err, res, body) {
+        if (err) console.error(err);
+        var $ = cheerio.load(body);
+        $('.lister-item-content').each(function(i, elem) {
+            var title = $(this).children('.lister-item-header').children('a').text();
+            title += ' ' + $(this).children('.lister-item-header').children('span').last().text();
+            var link = $(this).children('.lister-item-header').children('a').attr('href');
+            var rating = $(this).children('.ratings-bar').children('.ratings-imdb-rating').attr('data-value');
+            var movie = {
+                'title': title,
+                'link': domain + link,
+                'rating': rating
+            };
+            json['list'].push(movie);
+        });
+        //json['raw_html'] = body;
+        var next_url = $('.lister-page-next').attr('href');
+        json['next_url'] = domain + '/search/title' + next_url;
+
+        callback(null, json);
+    });
+}
+
+function scrape_list(json, callback) {
     async.waterfall([
         function(callback) {
-            request(url, function(err, res, body) {
-                if (err) console.error(err);
-                var $ = cheerio.load(body);
-                $('.lister-item-content').each(function(i, elem) {
-                    var title = $(this).children('.lister-item-header').children('a').text();
-                    title += ' ' + $(this).children('.lister-item-header').children('span').last().text();
-                    var link = $(this).children('.lister-item-header').children('a').attr('href');
-                    var rating = $(this).children('.ratings-bar').children('.ratings-imdb-rating').attr('data-value');
-                    json[i] = {
-                        'title': title,
-                        'link': domain + link,
-                        'rating': rating
-                    }
-                });
-                json['raw_html'] = body;
-                var next_url = $('.lister-page-next').attr('href');
-                json['next_url'] = {'link': domain + '/search/title' + next_url, 'title': 'next'}
-
-                callback(null, json);
-            });
+            callback(null, json);
         },
-        function(prevJson, callback) {
-            var newUrl = prevJson['next_url']['link'];
-            const prevCount = Object.keys(prevJson).length;
-            request(newUrl, function(err, res, body) {
-                if (err) console.error(err);
-                var $ = cheerio.load(body);
-                $('.lister-item-content').each(function(i, elem) {
-                    var title = $(this).children('.lister-item-header').children('a').text();
-                    title += ' ' + $(this).children('.lister-item-header').children('span').last().text();
-                    var link = $(this).children('.lister-item-header').children('a').attr('href');
-                    var rating = $(this).children('.ratings-bar').children('.ratings-imdb-rating').attr('data-value');
-                    prevJson[i + prevCount] = {
-                        'title': title,
-                        'link': domain + link,
-                        'rating': rating
-                    }
-                });
-                json['raw_html'] = body;
-                var next_url = $('.lister-page-next').attr('href');
-                json['next_url'] = {'link': domain + '/search/title' + next_url, 'title': 'next'}
-
-                callback(null, prevJson);
-            });
-        }
+        scrape_list_page,
+        scrape_page
     ], function(err, result) {
         callback(null, result);
     });
 }
 
-function scrape_page(url, callback) {
-    request(url, function(err, res, body) {
-        if (err) console.error(err);
-        var $ = cheerio.load(body);
-        var json = {
-            'raw_html': body,
-            'Title': $('.title_wrapper').children('h1').text(),
-            'Release': $('.subtext').children().last().text(),
-            'Rating': $('.ratingValue').text(),
-            'Rating Count': $('span[itemprop="ratingCount"]').text(),
-            'Genre': function() {
-                var line = $('div[itemprop="genre"]').children().not('.inline').text();
-                var values = line.split("| ");
-                return values;
-            }(),
-            'Content Rating': function() {
-                var line = $('.subtext').text();
-                var value = line.substring(0, line.indexOf('|'));
-                return value;
-            }(),
-        };
-        // Get directors, writers, and actors from /fullcredits
-        if (callback) callback(null, json);
-        //callback(null, json);
-    });
-}
 
 exports.index = function(req, res, next) {
+    var json = {'next_url':  list, 'movies': [], 'list': []};
     async.parallel({
-        scrape_data: function(callback) {
-            scrape_page("http://www.imdb.com/title/tt0111161/", callback);
-        },
         scrape_list: function(callback) {
-            //scrape_list(list, console.log);
-            scrape_list(list, callback);
+            //scrape_list(json, console.log);
+            scrape_list(json, callback);
         }
     }, function(err, results) {
         res.render('index', {
