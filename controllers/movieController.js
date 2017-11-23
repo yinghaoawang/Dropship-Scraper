33c83ec5747870778bf41a5c2e4772f7270dd0dd
Yinghao Wang <yinghao.a.wang@gmail.com>
2017-11-22 19:01:11 -0800

    jquery autocomplete working with db

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -6,13 +6,17 @@ var db = require('mongoose').connection;
 exports.index = function(req, res, next) {
     async.parallel({
         movies: function(callback) {
-            Movie.find({}).exec(callback);
-        }
+            Movie.find().exec(callback);
+        },
     }, function(err, results) {
         if (err) next(err);
-        console.log(results.movies.length);
+        var movie_names = [];
+        for (var i = 0; i < results.movies.length; ++i) {
+            //movie_names += '\'' + results.movies[i].title + '\',';
+            movie_names.push(results.movies[i].title);
+        }
         res.render('index', {
-            title: 'Index', error: err, movies: results.movies
+            title: 'Index', error: err, movies: results.movies, movie_names: movie_names
         });
     });
 };
